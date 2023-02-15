ec6b161a73fc0e02eb1a6beaf9902c28df2c6b88
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-16 04:00:42 +0600

    total done

--- app.js
+++ app.js
@@ -13,24 +13,50 @@ function totalPrice(idOne,idTwo,idThree){
     const price = document.getElementById(idThree);
 
     price.innerText = totalPrice;
+
+    return totalPrice;
+
 }
-
-
 
 document.getElementById('kitkat-buy-btn').addEventListener('click',function(){
 
-    totalPrice('kitkat-quantity','kitkat-price','chocolate')
+  const price = totalPrice('kitkat-quantity','kitkat-price','chocolate');
+
+  totalIs();
 
 });
 
 document.getElementById('rose-buy-btn').addEventListener('click',function(){
 
-    totalPrice('rose-quantity','rose-price','rose')
+ const price = totalPrice('rose-quantity','rose-price','rose');
+
+ totalIs();
 
 });
 
 document.getElementById('diary-buy-btn').addEventListener('click',function(){
 
-    totalPrice('diary-quantity','diary-price','diary')
+   const price =  totalPrice('diary-quantity','diary-price','diary');
+
+   totalIs();
 
 });
+
+
+
+function totalIs(){
+
+  const total = document.getElementById('total');
+
+  const priceRose = document.getElementById('rose').innerText;
+
+  const priceDiary = document.getElementById('diary').innerText; 
+
+  const priceKitkat = document.getElementById('chocolate').innerText;
+
+  const totals = parseInt(priceDiary) + parseInt(priceRose) + parseInt(priceKitkat); 
+
+  total.innerText = totals;
+
+}
+
